9a3485274ee24a33da60673b423752d82197ca60
J1ne_D03 <84855257+MarKir3656@users.noreply.github.com>
2026-04-16 21:01:21 +0300

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -15,23 +15,28 @@
 
 import argparse
 import asyncio
+import datetime
 # import socket
 # import dataclasses
 # import typing
 
 def pars_args():
     parser = argparse.ArgumentParser(description='port-scanner')
-    #parser.add_argument('-t', '--target')
     parser.add_argument('-t', '--target', required=True)
     parser.add_argument('-p', '--ports', default='1-1024',
                         help='port range like 1-1000 or 22,80,443')
     parser.add_argument('-T', '--threads', type=int, default=50)
     parser.add_argument('--timeout', type=int, default=3, help='time (in seconds) to wait before closing connection {default: %(default)s}')
+    parser.add_argument('-w', '--write', action='store_true', help="save scan results to file")
     args = parser.parse_args()
 
     ports_list = parse_port_range(args.ports)
 
     return args, ports_list
+
+def load_rules(portlist):
+
+    pass
 
 def parse_port_range(port_string: str) -> list:
     ports = []
@@ -48,6 +53,18 @@ def parse_port_range(port_string: str) -> list:
         
     return ports
 
+def save_results(results, rules):
+
+    filename = f"portscan_{datetime.now():%Y%m%d_%H%M%S}.txt"
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write(f"Scan results - {datetime.now()}\n")
+        f.write("-"*50 + "\n")
+        for port, (is_open, banner) in results:
+            if is_open:
+                desc = rules.get(port, {}).get("description", "unknown")
+                f.write(f"[+] {port}/tcp open {banner[:50]}    {desc} \n")
+
+    print(f"Result saved in {filename}")
 
 async def scan_ports(host, ports, max_concurrent, timeout):
     
@@ -69,7 +86,8 @@ async def check_port(host, port, timeout):
             timeout = timeout
         )
  
-        greeting = await reader.read(1024)
+        greeting = await grab(reader, writer, port)
+        
         writer.close()
         await writer.wait_closed()
         return True, greeting
@@ -80,16 +98,25 @@ async def check_port(host, port, timeout):
     except Exception as e:
         return False, str(e)
 
-# def grab(host, port):
+async def grab(host, port):
 
-#     greeting = reader.read
+
+    pass
 
 
 def main():
 
     args, ports = pars_args()
-    
-    asyncio.run(scan_ports(args.target, ports, args.threads, args.timeout))
+    rules = load_rules("porlist.txt")
+
+    results = asyncio.run(scan_ports(args.target, ports, args.threads, args.timeout))
+
+    for port, (is_open, banner) in results:
+        desc = rules.get(port, {}).get("description", "unknown")
+        print(f"[+] {port}/tcp open {banner[:50]}    {desc}")
+
+    if args.write:
+        save_results(results,rules)
 
 if __name__ == "__main__":
     main()
